23517cc41b04e8e3edb6c6c1839f690b0b139811
Angel Martinez <angel.martineztrc@gmail.com>
2021-04-02 14:59:06 -0600

    fix(AppReducer):  remove emptys strings in the names array

--- src/context/AppReducer.ts
+++ src/context/AppReducer.ts
@@ -4,7 +4,12 @@ import { ActionTypes } from '../types/ActionTypes';
 const AppReducer = (state: InitialStateValues, action: ActionTypes) => {
   switch (action.type) {
     case 'SET_INITIAL_VALUES':
-      return { ...state, ...action.payload };
+      return {
+        ...state,
+        title: action.payload.title,
+        // Remove emptys strings
+        names: [...action.payload.names.filter((el: string) => el !== '')]
+      };
     default:
       return state;
   }
